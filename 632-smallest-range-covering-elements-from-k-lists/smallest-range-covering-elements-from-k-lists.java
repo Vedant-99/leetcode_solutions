64410003f12f2fd7f38f4369153bb3cffed8a4d4
Vedant Agarwal <v.agarwal0309@gmail.com>
2026-07-25 01:11:40 +0530

    Time: 103 ms (31.94%) | Memory: 86.3 MB (38.91%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.java
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.java
@@ -1,45 +1,41 @@
 class Solution {
     class Triplet implements Comparable<Triplet>{
-        int value;  //value → actual value
-        int row;    //row → which list it came from
-        int col;    //col → index inside that list
-        Triplet(int value,int row,int col){
-            this.value = value;
+        int val;
+        int row;
+        int col;
+        Triplet(int val,int row,int col){
+            this.val = val;
             this.row = row;
             this.col = col;
         }
         public int compareTo(Triplet t){
-            return Integer.compare(this.value,t.value);
+            return Integer.compare(this.val,t.val);
         }
-
-
     }
     public int[] smallestRange(List<List<Integer>> nums) {
-        PriorityQueue<Triplet> pq = new PriorityQueue<>();
-        int k = nums.size();
-        int max = Integer.MIN_VALUE;
-
-        for(int i=0;i<k;i++){
-            int val = nums.get(i).get(0);
-            pq.add(new Triplet(val,i,0));
-            max = Math.max(val,max);
+       PriorityQueue<Triplet> pq = new PriorityQueue<>();
+       int k = nums.size();
+       int max = Integer.MIN_VALUE;
+       for(int i=-0;i<k;i++){
+        int value = nums.get(i).get(0);
+        pq.add(new Triplet(value,i,0));
+        max = Math.max(max,value);
+       }
+       int start = pq.peek().val;
+       int end = max;
+       while(true){
+        Triplet curr = pq.poll();
+        int min = curr.val;
+        if(max-min < end -start){
+            start = min;
+            end = max;
         }
-        int start = pq.peek().value;
-        int end = max;
-        while(true){
-            Triplet curr = pq.remove();
-            int min = curr.value;
-            if(max-min < end-start){
-                start = min;
-                end = max;
-            }
-            int nextCol = (curr.col+1);
-            if(nextCol == nums.get(curr.row).size()) break;
-            int nextVal = nums.get(curr.row).get(nextCol);
-            pq.add(new Triplet(nextVal,curr.row,nextCol));
-            max = Math.max(max,nextVal);
-        }
-        return new int[]{start,end};
-
+    int nextCol = curr.col+1;
+    if(nextCol == nums.get(curr.row).size()) break;
+    int nextVal = nums.get(curr.row).get(nextCol);
+    pq.add(new Triplet(nextVal,curr.row,nextCol));
+    max = Math.max(max,nextVal);
+       }
+       return new int[]{start,end}; 
     }
 }
