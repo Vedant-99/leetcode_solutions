9d30ffb4d19579f209cd98ec6daabf5addf8f313
Vedant Agarwal <v.agarwal0309@gmail.com>
2026-07-31 00:02:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (57.29%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,17 +1,17 @@
 class Solution {
-    int dp[];
+    int[] dp;
     public int rob(int[] nums) {
-        dp = new int[nums.length+1];
+        dp=new int[nums.length];
         Arrays.fill(dp,-1);
-        return loot(0,nums);
+        return loot(nums);
     }
-    public int loot(int idx,int arr[]){
-        if(idx>=arr.length) return 0;
-        if(dp[idx]!=-1) return dp[idx];
-        int pick = arr[idx]+loot(idx+2,arr);
-        int skip = loot(idx+1,arr);
-        int ans = Math.max(pick,skip);
-        dp[idx]=ans;
-        return ans;
+    public int loot(int[]nums){
+        // if(idx>=nums.length) return 0;
+        dp[0]=nums[0];
+       if(nums.length>1)dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length;i++){
+            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[nums.length-1];
     }
 }
