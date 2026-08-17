ab9737c3016c6db929c251bc5b6ed3745b31ace8
Vedant Agarwal <v.agarwal0309@gmail.com>
2026-08-17 10:52:08 +0530

    Time: 4 ms (98.51%) | Memory: 54.8 MB (91.26%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  if(s.length!==t.length) return false;
+  const freq= new Array(26).fill(0);
+
+  for(let i=0;i<s.length;i++){
+    freq[s.charCodeAt(i)-97]++;
+    freq[t.charCodeAt(i)-97]--;
+  }
+  for(let i=0;i<26;i++){
+    if(freq[i]!==0) return false; 
+  } 
+  return true;
+};
