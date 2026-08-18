5f0210077239e6a0075cd2a9832193da14feaf40
Vedant Agarwal <v.agarwal0309@gmail.com>
2026-08-19 00:52:54 +0530

    Time: 24 ms (34.15%) | Memory: 54.1 MB (20.82%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -4,9 +4,9 @@ class Solution {
         int m = text2.length();
         int[][] dp = new int[n][m];
         for(int[] row:dp) Arrays.fill(row,-1);
-        return helper(n-1,m-1,new StringBuilder(text1),new StringBuilder(text2),dp);
+        return helper(n-1,m-1,text1,text2,dp);
     }
-    public int helper(int n,int m,StringBuilder a,StringBuilder b,int[][] dp){
+    public int helper(int n,int m,String a,String b,int[][] dp){
         if(n<0 || m<0) return 0;
         if(dp[n][m]!=-1) return dp[n][m];
         if(a.charAt(n)==b.charAt(m)) return dp[n][m]=1+helper(n-1,m-1,a,b,dp);
