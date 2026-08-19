7befce737d12877ed8d8a141d82ddea3feb752ee
Vedant Agarwal <v.agarwal0309@gmail.com>
2026-08-19 14:22:17 +0530

    Time: 2 ms (100.00%) | Memory: 47.4 MB (6.00%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -2,22 +2,22 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int len1 = word1.length();
         int len2 = word2.length();
-        int[][] dp =new int[len1+1][len2+1];
+        int[][] dp =new int[len1][len2];
         for(int[] row:dp)Arrays.fill(row,-1);
-        return solve(word1,word2,len1,len2,dp);
+        return solve(len1-1,len2-1,word1,word2,dp);
     }
-    public int solve(String s,String t,int i,int j,int[][] dp){
-        if(i==0) return j;
-        if(j==0) return i;
+    public int solve(int i,int j,String s,String t,int[][] dp){
+        if(i<0) return j+1; // Inserting remaining characters of t
+        if(j<0) return i+1; // Deleteing extra characters of s
         if(dp[i][j]!=-1) return dp[i][j];
 
-        if(s.charAt(i-1)==t.charAt(j-1)){
-            return dp[i][j]=solve(s,t,i-1,j-1,dp);
+        if(s.charAt(i)==t.charAt(j)){
+            return dp[i][j]=solve(i-1,j-1,s,t,dp);
         }
 
-        int insertCost = solve(s,t,i,j-1,dp);
-        int deleteCost = solve(s,t,i-1,j,dp);
-        int replaceCost = solve(s,t,i-1,j-1,dp);
+        int insertCost = solve(i,j-1,s,t,dp);
+        int deleteCost = solve(i-1,j,s,t,dp);
+        int replaceCost = solve(i-1,j-1,s,t,dp);
 
         return dp[i][j]=1+Math.min(insertCost,Math.min(deleteCost,replaceCost));
     }
