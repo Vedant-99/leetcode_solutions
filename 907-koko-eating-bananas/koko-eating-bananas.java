85e40274823e8c252be14b7fb1c03e65d1f5fa0e
Vedant Agarwal <v.agarwal0309@gmail.com>
2026-08-31 00:25:20 +0530

    Time: 12 ms (49.80%) | Memory: 47.6 MB (87.96%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int max = Integer.MIN_VALUE;
+        for(int ele :piles) max = Math.max(max,ele);
+
+        int low =1,high = max, ans=-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(isPossible(mid,piles)<=h){
+                ans = mid;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        return ans;
+    }
+    public long isPossible(int speed,int[] arr){
+        long hours =0;
+        for(int ele:arr){
+            if(ele%speed == 0) hours+=ele/speed;
+            else hours+=(ele/speed+1);
+
+        }
+    return hours;
+    }
+}
